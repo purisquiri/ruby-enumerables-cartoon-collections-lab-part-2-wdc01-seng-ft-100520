8b009a56fadbcd86930e115ebb5537e4ed3d1234
Mauro Dorigo <mau.dorigo@outlook.es>
2020-09-03 16:41:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,8 +20,8 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  
   planeteer_calls.find do |call|
+  binding.pry
    call == valid_calls
   end
 end
